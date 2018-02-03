f7ebea3f565b428ccca0464b466138afec353301
Mateus Henrique Brum <mateushenriquebrum@gmail.com>
2018-02-03 20:56:27 -0200

    Inital commit

---
+++ src/domain/stock.js
@@ -0,0 +1,38 @@
+const emptyExchanger = Promise.resolve(1)
+
+const stock = (products, code, exchanger = emptyExchanger) => {
+    const stock = products
+    .byCode(code)
+    .then(products => {
+        return products.reduce((stock, product) => {
+            const price = stock.price + product.price
+            const cost = stock.cost + product.cost
+            const gain = price - cost
+            return {
+                price,
+                cost,
+                gain
+            }
+        }, {
+            cost: 0.0,
+            price: 0.0,
+            gain: 0.0
+        })
+    })
+
+    return Promise
+    .all([stock,exchanger])
+    .then(([{price,cost,gain},rate])=>{
+        console.log(cost)
+        console.log(rate)
+        return {
+            price : price*rate,
+            cost : cost*rate,
+            gain : gain*rate,
+        }
+    })
+}
+
+module.exports = {
+    stock
+}
